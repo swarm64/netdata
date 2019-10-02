028a8ce52406de2e26ed28d4fcbdfe70012a453b
Ashley Fraser <ashley@swarm64.com>
2019-10-02 14:50:25 +0200

    Working temperature and power readings

--- fpga.chart.py
+++ fpga.chart.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# Description: python module to read data from loca FPGAs
-# Author: Luc Vlaming, Ashley Fraser
-# SPDX-License-Identifier: MIT
+# Description: example netdata python.d module
+# Author: Put your name here (your github login)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import time
 
@@ -14,10 +14,37 @@ import re
 
 priority = 90000
 
+FPGA_COUNT = 2
+
 BYTE_DEFINITION = {
     'options': [None, 'Transfered data', 'MB/sec', 'fpga', 'fpga', 'line'],
     'lines': [['host_to_fpga_byte_count', 'sent to fpga', 'incremental', 1, 1024*1024],
               ['fpga_to_host_byte_count', 'received from fpga', 'incremental', -1, 1024*1024]]
+}
+JOB_DEFINITION = {
+    'options': [None, 'Processed jobs', 'Jobs/sec', 'fpga', 'fpga', 'line'],
+    'lines': [['compression_job_count', 'compressed jobs', 'incremental'],
+              ['decompression_job_count', 'decompressed jobs', 'incremental'],
+              ['decompression_and_filter_job_count', 'decompressed and filtered jobs', 'incremental'],
+              ['filter_job_count', 'filtered jobs', 'incremental']
+             ]
+}
+MAX_DEFINITION = {
+    'options': [None, 'Max outstanding jobs', 'max oustanding', 'fpga', 'fpga', 'line'],
+    'lines': [['max_outstanding_compression_jobs', 'compression', 'absolute'],
+              ['max_outstanding_decompression_and_filter_jobs', 'decompress and filter', 'absolute'],
+              ['max_outstanding_filter_jobs', 'filter', 'absolute']
+             ]
+}
+TEMP_DEFINITION = {
+    'options': [None, 'FPGA Temperature', '°C', 'fpga', 'fpga', 'line'],
+    'lines': [['temperature', 'Degrees Celcius', 'absolute']
+             ]
+}
+POWER_DEFINITION = {
+    'options': [None, 'FPGA Power Consumption', 'Watts', 'fpga', 'fpga', 'line'],
+    'lines': [['power', 'Total Watts', 'absolute']
+             ]
 }
 
 class Service(SimpleService):
@@ -31,16 +58,43 @@ class Service(SimpleService):
         self.fpga_mapping = dict()
         self.next_fpga = 0
         self.dsn = self.configuration.get('dsn')
-        self.fpga_count = self.configuration.get('fpga_count')
 
-        for i in range(self.fpga_count):
+        for i in range(FPGA_COUNT):
             name = 'fpga-' + str(i)
             bytes = name + '-bytes'
+            jobs = name + '-jobs'
+            maxes = name + '-max'
+            temps = name + '-temperature'
+            powers = name + '-power'
             self.order.append(bytes)
+            self.order.append(jobs)
+            self.order.append(maxes)
+            self.order.append(temps)
+            self.order.append(powers)
             self.definitions[bytes] = copy.deepcopy(BYTE_DEFINITION)
             self.definitions[bytes]['options'][3] = name
+            self.definitions[jobs] = copy.deepcopy(JOB_DEFINITION)
+            self.definitions[jobs]['options'][3] = name
+            self.definitions[maxes] = copy.deepcopy(MAX_DEFINITION)
+            self.definitions[maxes]['options'][3] = name
+            self.definitions[temps] = copy.deepcopy(TEMP_DEFINITION)
+            self.definitions[temps]['options'][3] = name
+            self.definitions[powers] = copy.deepcopy(POWER_DEFINITION)
+            self.definitions[powers]['options'][3] = name
 
             for key in self.definitions[bytes]['lines']:
+                key[0] = name + '-' + key[0]
+                self.keys.append(key[0])
+            for key in self.definitions[jobs]['lines']:
+                key[0] = name + '-' + key[0]
+                self.keys.append(key[0])
+            for key in self.definitions[maxes]['lines']:
+                key[0] = name + '-' + key[0]
+                self.keys.append(key[0])
+            for key in self.definitions[temps]['lines']:
+                key[0] = name + '-' + key[0]
+                self.keys.append(key[0])
+            for key in self.definitions[powers]['lines']:
                 key[0] = name + '-' + key[0]
                 self.keys.append(key[0])
  
@@ -64,6 +118,25 @@ class Service(SimpleService):
 
         return True
 
+    def _parse_fpgainfo(self, cmd, re_string):
+        fpga_info_res = subprocess.check_output(cmd, shell=True)
+        fpga_info_out = fpga_info_res.split(b'\n')
+
+        for line in fpga_info_out:
+            info_line = re.match(re_string, line.decode('UTF-8'))
+            if info_line is not None:
+                return info_line.group(1)
+
+    def _get_fpga_temp(self):
+        FPGA_TEMPERATURE_CMD = "/usr/bin/fpgainfo temp"
+        RE_TEMP_STRING = r'^.*FPGA Core TEMP \s+: (\d+)'
+        return self._parse_fpgainfo(FPGA_TEMPERATURE_CMD, RE_TEMP_STRING)
+
+    def _get_fpga_power(self):
+        FPGA_POWER_CMD = "/usr/bin/fpgainfo power"
+        RE_POWER_STRING = r'^.*Total Input Power \s+: (\d+)\.'
+        return self._parse_fpgainfo(FPGA_POWER_CMD, RE_POWER_STRING)
+
     def get_data(self):
         self._connect(self.dsn)
 
@@ -71,8 +144,11 @@ class Service(SimpleService):
         if not self.conn:
             return self.default_data
 
+
         try:
             data = copy.deepcopy(self.default_data)
+            data['fpga-0-temperature'] = self._get_fpga_temp()
+            data['fpga-0-power'] = self._get_fpga_power()
 
 
             with self.conn.cursor() as cursor:
@@ -97,6 +173,7 @@ class Service(SimpleService):
                         name = fpga_key + '-' + column.name
                         if name in self.keys:
                             data[name] = row[columns[column.name]]
+
 
 
                 return data
