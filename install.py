2b56c0a929c3cca78164f0960285d28afaeb5efb
Ashley Fraser <ashley@swarm64.com>
2019-11-01 12:26:10 +0100

    Adde args fro non-default paths

--- install.py
+++ install.py
@@ -4,11 +4,10 @@ import re
 import os
 import subprocess
 import shutil
+import argparse
 
 
-CHART_DIR = '/usr/ibexec/netdata/python.d'
 CHART = './fpga.chart.py'
-CONF_DIR = '/etc/etdata/python.d'
 CONF = './fpga.conf'
 
 
@@ -40,18 +39,23 @@ def netdata_is_installed(linux_flavour):
         return False
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get possible non-default paths for config and driver')
+    parser.add_argument('--config', default='/etc/netdata/python.d', help='Where the netdata config file is located')
+    parser.add_argument('--python-chart', default='/usr/libexec/netdata/python.d', help='Where the python chart file is located')
+    args = parser.parse_args()
+
     linux_flavour = get_flavour()
 
     if netdata_is_installed(linux_flavour):
-        if os.path.isdir(CHART_DIR):
-            shutil.copy(CHART, CHART_DIR)
+        if os.path.isdir(args.python_chart):
+            shutil.copy(CHART, args.python_chart)
         else:
-            print("Couldn't copy chart file to {}".format(CHART_DIR))
+            print("Couldn't copy chart file to {}".format(args.python_chart))
 
-        if os.path.isdir(CONF_DIR):
-            shutil.copy(CONF, CONF_DIR)
+        if os.path.isdir(args.config):
+            shutil.copy(CONF, args.config)
         else:
-            print("Couldn't copy conf file to {}".format(CONF_DIR))
+            print("Couldn't copy conf file to {}".format(args.config))
 
     else:
         print("Netdata is not installed, please install it first.")
